3963194b4403354de3b9d608358d9079ec58482a
Archisman Saha <archisman183@gmail.com>
2026-05-03 13:46:42 +0530

    Plant spawning from SO

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,16 @@ using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public static GameManager Instance { get; private set; }
     [SerializeField] private float globalCurrentDay = 1;
+
+    private void Awake() {
+        if (Instance != null && Instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
 
     private void Start() {
         DayNightManager.Instance.dayEnd += OnDayEnd;
--- Assets/Scripts/PlantInstance.cs
+++ Assets/Scripts/PlantInstance.cs
@@ -1,10 +1,41 @@
+using System;
 using UnityEngine;
 
 public class PlantInstance : MonoBehaviour {
     public PlantSO plantData;
-    private int currentAge;
+    private int currentAge = 0;
+    private GameObject spawnedPlant;
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
 
     private void Start() {
-        
-    }    
+        spawnPosition = Vector3.zero;
+        float randomY = UnityEngine.Random.Range(0f, 360f);
+        spawnRotation = Quaternion.Euler(0, randomY, 0);
+        DayNightManager.Instance.dayEnd += OnDayEndPlant;
+        spawnedPlant = Instantiate(plantData.growthStages[currentAge], spawnPosition, spawnRotation, transform);
+        spawnedPlant.transform.localPosition = spawnPosition; 
+        spawnedPlant.transform.localRotation = spawnRotation;
+    }
+    private void OnDisable() {
+        DayNightManager.Instance.dayEnd -= OnDayEndPlant;
+    }
+
+    private void OnDayEndPlant(object sender, EventArgs e) {
+        if (spawnedPlant != null) {
+            currentAge++;
+            if (currentAge >= plantData.daysToGrow) return;
+            UpdatePrefab();
+        } else {
+            Debug.Log("No plant prefab found!");
+        }
+    }
+
+    private void UpdatePrefab() {
+        Destroy(spawnedPlant); // Removes it from the scene
+        spawnedPlant = Instantiate(plantData.growthStages[currentAge], spawnPosition, spawnRotation, transform);
+        spawnedPlant.transform.localPosition = spawnPosition; 
+        spawnedPlant.transform.localRotation = spawnRotation;
+    }
+
 }
